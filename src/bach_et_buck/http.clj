070d2e6335e68e313416f9984ba75a57e1b4ae3b
Romain Jysch <rjh-network@pm.me>
2024-04-30 09:10:11 +0200

    feat(clj-bach-et-buck): Return customers from DB in a JSON format

--- src/bach_et_buck/http.clj
+++ src/bach_et_buck/http.clj
@@ -1,9 +1,9 @@
 (ns bach-et-buck.http
-  (:require [bach-et-buck.db :as db]
-            [clojure.data.json       :as json]))
+  (:require [bach-et-buck.db   :as db]
+            [clojure.data.json :refer [write-str]]))
 
 (defn get-customers
   [request]
   {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "Coucou"})
+   :headers {"Content-Type" "application/json"}
+   :body    (str (write-str (db/get-customers)))})
