a0ebc73d97aeb87cf5254a557f7981f258abbdf8
Romain Jysch <rjh-network@pm.me>
2024-05-06 13:35:31 +0200

    feat(clj-bach-et-buck): Add datasource and system files

---
+++ src/bach_et_buck/datasource.clj
@@ -0,0 +1,21 @@
+(ns bach-et-buck.datasource
+  (:require [bach-et-buck.db            :as db]
+            [com.stuartsierra.component :as component])
+  (:import  [com.zaxxer.hikari HikariConfig HikariDataSource]))
+
+(defn hikari-config [config]
+  (let [{:keys [jdbc-url user password]} config]
+    (doto (HikariConfig.)
+      (.setJdbcUrl jdbc-url)
+      (.setUsername user)
+      (.setPassword password))))
+
+(defrecord Datasource [config]
+  component/Lifecycle
+  (start [this]
+    (let [datasource (HikariDataSource. (hikari-config config))]
+      (merge this {:datasource datasource
+                   :schema db/schema
+                   :jdbc datasource})))
+  (stop [{:keys [datasource]}]
+    (.close datasource)))
