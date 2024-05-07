5b09e436cc418b91453b8e6a821d20e1b65042df
Romain Jysch <rjh-network@pm.me>
2024-05-07 09:01:06 +0200

    refactor(clj-bach-et-buck): Specify Content-Type and add a JSON middleware

--- src/bach_et_buck/server.clj
+++ src/bach_et_buck/server.clj
@@ -2,19 +2,25 @@
   (:require [bach-et-buck.service       :as service]
             [com.stuartsierra.component :as component]
             [clojure.data.json          :refer [write-str]]
+            [clojure.tools.logging      :refer [info]]
+            [compojure.core             :refer [routes GET]]
             [ring.adapter.jetty         :refer [run-jetty]]
-            [compojure.core             :refer [routes GET]]))
+            [ring.middleware.json       :refer [wrap-json-body wrap-json-response]]))
 
-(defn make-handler
-  [service]
+(defn make-handler [service]
   (routes
-    (GET "/customers" [] (write-str (service/find-customers service)))))
+        (GET "/customers" [] (info "GET /customers asked.")
+                             {:status  200
+                              :headers {"Content-Type" "application/json"}
+                              :body    (write-str (service/find-customers service))})))
 
 (defrecord Server [config
                    service] ;; dependency
   component/Lifecycle
   (start [this]
-    (assoc this :server (run-jetty (#'make-handler service)
+    (assoc this :server (run-jetty (-> (#'make-handler service)
+                                       wrap-json-response
+                                       wrap-json-body)
                                    {:join? false
                                     :port (get config :port 11000)})))
   (stop [this]
