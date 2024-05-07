33977791004744a9c5f588c7fffc433ae37b030c
Romain Jysch <rjh-network@pm.me>
2024-05-07 11:19:00 +0200

    feat(clj-bach-et-buck): Add 400 Bad Request when when the cardnumber does not exists

--- src/bach_et_buck/server.clj
+++ src/bach_et_buck/server.clj
@@ -2,7 +2,7 @@
   (:require [bach-et-buck.service       :as service]
             [com.stuartsierra.component :as component]
             [clojure.data.json          :refer [write-str]]
-            [clojure.tools.logging      :refer [info]]
+            [clojure.tools.logging      :refer [info error]]
             [compojure.core             :refer [routes GET]]
             [ring.adapter.jetty         :refer [run-jetty]]
             [ring.middleware.json       :refer [wrap-json-body wrap-json-response]]))
@@ -16,10 +16,18 @@
            :body    (write-str (service/find-all-customers service))})
 
         (GET "/customer/:cardnumber" [cardnumber]
-          (info "GET /customer/:cardnumber asked for" cardnumber)
-          {:status  200
-           :headers {"Content-Type" "application/json"}
-           :body    (write-str (service/find-customer-by-cardnumber service cardnumber))})))
+          (let [customer (service/find-customer-by-cardnumber service cardnumber)]
+            (if (not-empty customer)
+              (do
+                (info "GET /customer/:cardnumber asked for" cardnumber)
+                {:status  200
+                 :headers {"Content-Type" "application/json"}
+                 :body    (write-str customer)})
+              (do
+                (error "GET /customer/:cardnumber returns 400 for" cardnumber)
+                {:status 400
+                 :headers {"Content-Type" "text/html"}
+                 :body "400 Bad Request: The format or value of the card number is incorrect."}))))))
 
 (defrecord Server [config
                    service] ;; dependency
